10a8d03433a3621680e455b0fbb4d1eb9486e6a1
Kyle Burt <arrant-pettier2u@icloud.com>
2025-11-15 02:21:14 -0800

    Add event listener for page navigation

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ import "./home";
 import { buildHome } from "./home";
 
 buildHome();
+
+const nav = document.querySelector('nav');
+nav.addEventListener('click', (event) => {
+    switch(event.target.id) {
+        case ('home'):
+            buildHome();
+            break;
+
+        case ('menu'):
+
+        case ('about'):
+
+        default:
+            break;
+    }
+})
